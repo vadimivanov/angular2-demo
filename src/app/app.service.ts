61b64f95e9bb715bede37ed1633ee7c00f494c0b
vadimivanov <Vjcrdf0508>
2017-04-20 17:38:07 +0300

    added structure

---
+++ src/app/app.service.ts
@@ -0,0 +1,5 @@
+@Injectable($http)
+// http://api.openweathermap.org/data/2.5/weather?q=Kharkiv&APPID=41792d0a39a32256e92cc638d3e86a2c
+export class ServiceRequest {
+    getData(): void {} // stub
+}
